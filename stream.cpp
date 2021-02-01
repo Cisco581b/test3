5aa13de4a653c38065d731258b0d2e282bf9a1f0
Cisco581b <cisco581b@gmail.com>
2021-02-01 16:01:55 +0530

    Added some files

---
+++ stream.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+		{
+				ofstream of("test1.txt");
+                of<<"This is a test of ofstream in c++"<<endl;
+				return 0;
+		}
+}
